b6ec892389808f39bcc8c9af3f59d95fc685864a
ZR1995 <1589402810@qq.com>
2019-12-11 16:45:26 +0800

    feat(Dataset, signal_generation): create signal test for audio, zaidai, pulse, lfm, restore qammod

---
+++ DataSet.py
@@ -0,0 +1,75 @@
+import torch as t
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
+import h5py
+import numpy as np
+
+
+def load_data(train_data_dir):
+    print('loading train data set')
+    # x_train, y_train, x_valid, y_valid = [], [], [], []
+    # train_data_dir = './data/'
+    allData = h5py.File(train_data_dir, 'r')
+    x_train = np.transpose(allData['x_train']).astype('float64')
+    y_train = np.transpose(allData['y_train']).astype('float64')
+    x_valid = np.transpose(allData['x_valid']).astype('float64')
+    y_valid = np.transpose(allData['y_valid']).astype('float64')
+
+    return x_train, y_train, x_valid, y_valid
+
+
+def get_data(train_ds, valid_ds, bs):
+    return (
+        DataLoader(train_ds, batch_size=bs, shuffle=True),
+        DataLoader(valid_ds, batch_size=bs),
+    )
+
+
+# def get_model(options):
+# model = CustomizedNet(options.train_net_iter)
+# return model, t.optim.SGD(model.parameters(), lr=options.learning_rate, momentum=options.momentum)
+
+
+def loss_batch(model, loss_f, xb, yb, opt=None):
+    loss = loss_f(model(xb), yb)
+    if opt is not None:
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+    return loss.item(), len(xb)
+
+
+def preprocess(x, y):
+    return x.to('cuda'), y.to('cuda')
+
+
+class WrappedDataLoader:
+    def __init__(self, dl, func):
+        self.dl = dl
+        self.func = func
+
+    def __len__(self):
+        return len(self.dl)
+
+    def __iter__(self):
+        batches = iter(self.dl)
+        for b in batches:
+            yield self.func(*b)
+
+
+def ber(y_pred, y_target):
+    return t.mean(t.abs(y_pred - y_target))
+
+
+def load_train(train_data_dir, batch_size):
+    x_train, y_train, x_valid, y_valid = map(t.tensor, (load_data(train_data_dir)))
+    train_ds = TensorDataset(x_train, y_train)
+    valid_ds = TensorDataset(x_valid, y_valid)
+    train_dl, valid_dl = get_data(train_ds, valid_ds, batch_size)
+    train_dl = WrappedDataLoader(train_dl, preprocess)
+    valid_dl = WrappedDataLoader(valid_dl, preprocess)
+    return train_dl, valid_dl
+
+
+if __name__ == '__main__':
+    load_train(train_data_dir='./data/train_channel.mat', batch_size=128)
--- VTCCNN.py
+++ VTCCNN.py
@@ -1,3 +1,108 @@
-import torch as py
+# 目标是在无干扰的情况下训练一个CNN模型
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 import numpy as np
+import h5py
+from DataSet import load_train
 
+N = 62000  # 训练数据行数
+C = 3000
+classNum = 31
+matfn = 'F:/train/train14.mat'
+data = h5py.File(matfn, 'r')
+
+train_x = np.transpose(data['train_x'])
+train_y = np.transpose(data['train_y'])
+print("train_x.shape:", train_x.shape)
+print("train_y.shape:", train_y.shape)
+
+# 预处理随机打乱
+index = np.arange(N)
+np.random.shuffle(index)
+train_x = train_x[index, :]
+train_y = train_y[index]
+
+# 转换为tensor
+train_X = torch.from_numpy(train_x)
+train_y = torch.from_numpy(train_y)
+
+train_X = train_X.reshape((N, 1, C))  # 1为通道数量， 3000为长度
+train_y = train_y.reshape((N, 1))
+print(train_X.shape)
+print(train_y.shape)
+
+# torch one_hot 类型转换
+train_y = train_y.long()
+train_y_onehot = torch.zeros(N, classNum).scatter_(1, train_y, 1)
+
+batch_size = 128
+nb_classes = 10
+nb_epoch = 100
+momentum = 0.3
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class CNN(nn.Module):
+
+    def __init__(self):
+        super(CNN, self).__init__()
+
+        self.layer = nn.Sequential(
+            nn.Conv1d(1, 30, 9),
+            nn.ELU(),
+            nn.Conv1d(30, 25, 9, 3),
+            nn.ELU(),
+            nn.MaxPool1d(3, 2),
+            nn.Conv1d(24, 20, 9, 3),
+            nn.ELU(),
+            nn.Flatten(),
+            nn.Linear(3260, 200),
+            nn.Linear(200, 120),
+            nn.Linear(120, 60),
+            nn.Linear(60, 40),
+            nn.Linear(40, 31)
+        )
+        self.softmax = nn.Softmax()
+
+    def forward(self, x):
+        x = self.layer(x)
+        x = self.softmax(x)
+        return x
+
+
+net = CNN()
+net.to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=momentum)
+
+# 加载数据
+train_loader = load_train('./data/train_channel.mat', 128)
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data[0].to(device), data[1].to(device)
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 200 == 199:    # print every 200 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 200))
+            running_loss = 0.0
+
+print('Finished Training')
+
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
